e0c17effc8d8940efaf4e085d4234403d9872e59
nikishkaa <nikita.goutor@gmail.com>
2024-09-13 00:02:59 +0300

    Update.

---
+++ src/main/java/org/example/javaeedemo/filter/LoggerFilter.java
@@ -0,0 +1,27 @@
+package org.example.javaeedemo.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Date;
+
+@WebFilter(filterName = "LoggerFilter", urlPatterns = {"/*"})
+public class LoggerFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        System.out.println(new Date() + " -> Request:  " + httpServletRequest.getServletContext() + "/ " + httpServletRequest.getServerName()
+                + "/ " + httpServletRequest.getServerPort());
+        System.out.println(new Date() + " -> " + httpServletRequest.getRequestedSessionId() + "/ " + httpServletRequest.getSession());
+
+        chain.doFilter(request, response);
+    }
+}
