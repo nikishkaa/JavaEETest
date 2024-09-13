26d0f4f073e52cfeedd5173929e5c7975ff120f5
nikishkaa <nikita.goutor@gmail.com>
2024-09-13 20:23:42 +0300

    Hibernate.

---
+++ src/main/java/org/example/javaeedemo/test/HibernateMain.java
@@ -0,0 +1,33 @@
+package org.example.javaeedemo.test;
+
+
+import java.util.Date;
+
+import org.example.javaeedemo.entity.Employee;
+import org.example.javaeedemo.utils.HibernateUtil;
+import org.hibernate.Session;
+
+
+public class HibernateMain {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.setName("Pankaj");
+        emp.setRole("CEO");
+        emp.setInsertTime(new Date());
+
+        //Get Session
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        //start transaction
+        session.beginTransaction();
+        //Save the Model object
+        session.save(emp);
+        //Commit transaction
+        session.getTransaction().commit();
+        System.out.println("Employee ID=" + emp.getId());
+
+        //terminate session factory, otherwise program won't end
+        HibernateUtil.getSessionFactory().close();
+    }
+
+}
