4f9f5f2aec8fd8082779853c8dae196cad4b28cb
nikishkaa <nikita.goutor@gmail.com>
2024-09-14 18:19:16 +0300

    Hibernate.

--- src/main/java/org/example/javaeedemo/dao/impl/EmployeeDao.java
+++ src/main/java/org/example/javaeedemo/dao/impl/EmployeeDao.java
@@ -4,6 +4,10 @@ import org.example.javaeedemo.dao.AbstractDao;
 import org.example.javaeedemo.entity.Employee;
 
 public class EmployeeDao extends AbstractDao<Integer, Employee> {
-
+    public Employee findByEmail(String email) {
+        getSession();
+//TODO
+        return null;
+    }
 
 }
